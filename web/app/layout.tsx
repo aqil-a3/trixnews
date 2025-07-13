2939171cf1a51b69e79f5ce300da34e3a5c02320
aqil-a3 <aqilalumindo@gmail.com>
2025-07-13 14:14:17 +0700

    fix error

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
   generator: "v0.dev",
 };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function RootLayout({
   children,
 }: Readonly<{

9f3fc65edc697882b32278fe0a730310acd85f0b
aqil-a3 <aqilalumindo@gmail.com>
2025-07-15 08:53:59 +0700

    Update

--- web/lib/variables.ts
+++ web/lib/variables.ts
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 
-export const loginPageUrl = "https://trixnews-5oef.vercel.app/login";
+export const loginPageUrl = `${process.env.NEXT_PUBLIC_ADMIN_URL}`;
 export const sharedSecretKey = `${process.env.SHARED_SECRET}`;
 
 export const apiUrl = isDevelopment
